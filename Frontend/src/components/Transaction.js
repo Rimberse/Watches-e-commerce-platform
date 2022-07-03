38403482d8037166daff5dffa5778e3060f4017b
Rimberse <kerimpltm@gmail.com>
2022-07-03 15:45:02 +0200

    Implement fetching of transaction data on frontend, implement Transaction and Transaction-History components

--- Frontend/src/components/Transaction.js
+++ Frontend/src/components/Transaction.js
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
 import '../styles/Transaction.css';
+import Card from './Card';
 
 // Transaction card which used in unordered list to display thansaction informations
-const Transaction = ({ transaction }) => {
+const Transaction = ({ watch, client, quantity }) => {
+    
+    // Formats price to the given currency and region. Used to nicely format price of real estates
+    const formatter = new Intl.NumberFormat('fr-FR', {
+        style: 'currency',
+        currency: 'EUR'
+    });
 
     return(
         <>
-
+            <div className='transaction'>
+                <div className='transaction-watch'>
+                    <div className='transaction-watch-image'>
+                        <img src={ watch.Image } alt='Watch'></img>
+                    </div>
+                    <div className='transaction-watch-details'>
+                        <div className='transaction-watch-details-info'>
+                            <h2 className='transaction-watch-details-info-name'>{ watch.Name }</h2>
+                            <h3 className='transaction-watch-details-info-brand'><span>{ watch.Brand }</span></h3>
+                            <p className='transaction-watch-details-info-description'>{ watch.Description }</p>
+                            <h3 className='transaction-watch-details-info-price'>{ formatter.format(watch.Price) }</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='transaction-client'>
+                    <p className='transaction-client-firstName'>{ client.FirstName }</p>
+                    <p className='transaction-client-lastName'>{ client.LastName }</p>
+                    <p className='transaction-client-email'>{ client.email }</p>
+                </div>
+                <h2 className='transaction-quantity'>{ quantity }</h2>
+            </div>
         </>
     );
 }
--- Frontend/src/components/TransactionHistory.js
+++ Frontend/src/components/TransactionHistory.js
@@ -1,16 +1,60 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../styles/TransactionHistory.css';
 import transactionService from '../services/transaction';
+import Transaction from './Transaction';
 
 // Lets the admin to view number of purchases have been made by each client: Product information, quantity ordered, price, client
 const TransactionHistory = ({ user }) => {
-    const [transactions, SetTransactions] = useState([]);
+    const [transactions, setTransactions] = useState([]);
+    const [page, setPage] = useState(Number(1));
+
+    useEffect(() => {
+        transactionService.retrieve(page)
+            .then(initialTransactions => {
+                setTransactions(initialTransactions.data);
+                setPage(Number(initialTransactions.meta.page));
+            })
+    }, [page]);
+
+    const loadPrevPage = () => {
+        console.log("Pressed");
+        if (page <= 1)
+            return;
+    
+        setPage(page - 1);
+    }
+    
+    const loadNextPage = () => {
+        console.log("Pressed");
+        if (transactions.length < 10)
+            return;
+    
+        setPage(page + 1);
+    };
+
+    // Retrieves watch related informations from the list of transactions
+    const retrieveWatch = transaction => {
+        const watch = (({ IdWatches, Name, Type, Price, Brand, Weight, MaterialType, Color, WristSize, DialWatchType, CollectionName, Mechanism, Stock, Image, Description }) => 
+              ({ IdWatches, Name, Type, Price, Brand, Weight, MaterialType, Color, WristSize, DialWatchType, CollectionName, Mechanism, Stock, Image, Description }))(transaction);
+        
+        return watch;
+    }
+
+    // Retrieves client related informations from the list of transactions
+    const retrieveClient = transaction => {
+        const client = (({ IdCustomer, FirstName, LastName, Email }) => 
+              ({ IdCustomer, FirstName, LastName, Email }))(transaction);
+        
+        return client;
+    }    
 
     return(
         <>
             <h1 className='Transaction-history-header'>Transaction history</h1>
             { user === "Admin" && <div className='Transaction-history'>
-                
+                <ul>
+                    { transactions.map(transaction => <li key={ transaction.IdWatches } className='transaction-element'><Transaction watch={ retrieveWatch(transaction) } client={ retrieveClient(transaction) } quantity={ transaction.Quantity } /></li>) }
+                </ul>
             </div>}
         </>
     );
