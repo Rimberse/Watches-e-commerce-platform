0cad4e1157271a5aa570597093c78851132736ea
Rimberse <kerimpltm@gmail.com>
2022-07-04 00:13:06 +0200

    Initial commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Watches from './components/Watches';
 import Payment from './components/Payment';
 import TransactionHistory from './components/TransactionHistory';
 import './App.css';
-import Payment from './components/Payment';
 
 const App = () => {
+  const [user, setUser] = useState("Guest");  // Used to grand the admin right to perform CRUD operations
+  const [clientID, setClientID] = useState(0); // Used to get clientID from DB, which is used later on when client makes a purchase
   
   return (
     <div className="App">
